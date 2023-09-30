0aebaee608b1b88ee630c19aedda6a613363f480
Diki Dwi Hermawan <dikidwihermawan@gmail.com>
2023-09-30 10:47:56 +0700

    add auth

---
+++ src/view/auth/Register.jsx
@@ -0,0 +1,242 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+function Register() {
+  const redirect = useNavigate();
+
+  const [errors, setErrors] = useState([]);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPasswordConfirmation] = useState("");
+
+  const store = async (e) => {
+    e.preventDefault();
+    const data = { email, username, name, password, password_confirmation };
+
+    try {
+      let response = await axios.post("register", data);
+
+      toast.success(response.data.success, {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+
+      setEmail("");
+      setUsername("");
+      setName("");
+      setPassword("");
+      setPasswordConfirmation("");
+      return redirect("/login");
+    } catch (e) {
+      toast.error("Please fill in the form correctly!", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      setErrors(e.response.data.errors);
+    }
+  };
+
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-4 mx-auto lg:py-8">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Create and account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={store}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  value={email}
+                  type="email"
+                  name="email"
+                  id="email"
+                  className={`bg-gray-50 border ${
+                    errors.email ? "border-red-600" : "border-gray-300"
+                  } text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                  placeholder="name@gmail.com"
+                />
+                {errors.email ? (
+                  <span className="text-red-600 text-xs">{errors.email}</span>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your username
+                </label>
+                <input
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                  value={username}
+                  type="text"
+                  name="username"
+                  id="username"
+                  className={`bg-gray-50 border ${
+                    errors.username ? "border-red-600" : "border-gray-300"
+                  } text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                  placeholder="dikidh"
+                />
+                {errors.username ? (
+                  <span className="text-red-600 text-xs">
+                    {errors.username}
+                  </span>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your name
+                </label>
+                <input
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  value={name}
+                  type="text"
+                  name="name"
+                  id="name"
+                  className={`bg-gray-50 border ${
+                    errors.name ? "border-red-600" : "border-gray-300"
+                  } text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                  placeholder="Diki Dwi Hermawan"
+                />
+                {errors.name ? (
+                  <span className="text-red-600 text-xs">{errors.name}</span>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  value={password}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className={`bg-gray-50 border ${
+                    errors.password ? "border-red-600" : "border-gray-300"
+                  } text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                />
+                {errors.password ? (
+                  <span className="text-red-600 text-xs">
+                    {errors.password}
+                  </span>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="password_confirmation"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Confirm password
+                </label>
+                <input
+                  onChange={(e) => {
+                    setPasswordConfirmation(e.target.value);
+                  }}
+                  value={password_confirmation}
+                  type="password"
+                  name="password_confirmation"
+                  id="password_confirmation"
+                  placeholder="••••••••"
+                  className={`bg-gray-50 border ${
+                    errors.password_confirmation
+                      ? "border-red-600"
+                      : "border-gray-300"
+                  } text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                />
+              </div>
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    id="terms"
+                    aria-describedby="terms"
+                    type="checkbox"
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                  />
+                </div>
+                <div className="ml-3 text-sm">
+                  <label
+                    htmlFor="terms"
+                    className="font-light text-gray-500 dark:text-gray-300"
+                  >
+                    I accept the{" "}
+                    <a
+                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                      href="#"
+                    >
+                      Terms and Conditions
+                    </a>
+                  </label>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Create an account
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Already have an account?{" "}
+                <a
+                  href="/login"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Login here
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Register;
