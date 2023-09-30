5d37f04bb7f16df3ae6e1c3841fcb431a4bb332e
Diki Dwi Hermawan <dikidwihermawan@gmail.com>
2023-09-30 10:40:35 +0700

    add configure middleware

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import axios from "axios";
+import { RecoilRoot } from "recoil";
+import { BrowserRouter } from "react-router-dom";
+
+axios.defaults.baseURL = "http://127.0.0.1:8000/api";
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("tokenUser")}`;
+  return config;
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </BrowserRouter>
 );
